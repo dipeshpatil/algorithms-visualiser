7fe2d45f6c0e9b73f6807770387338a4256e97a8
dipeshpatil <thedipeshpatil@gmail.com>
2020-06-29 18:37:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,20 +24,18 @@ export default class App extends React.Component {
         return (
             <div className="App">
                 <div className="bg-dark py-2 px-2">
-                    <span className="text-light">
-                        Algorithms Visualiser
-                    </span>
+                    <span className="text-light">Algorithms Visualiser</span>
                     <span className="float-right">
                         <a
                             className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
-                            href="https:github.com/dipeshpatil/algorithms-visualiser"
+                            href="https://github.com/dipeshpatil/algorithms-visualiser"
                         >
                             <i className="fab text-light fa-github"></i>
                             &nbsp; Repo URL
                         </a>
                         <a
                             className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
-                            href="https:linkedin.com/in/dipesh-patil"
+                            href="https://linkedin.com/in/dipesh-patil"
                         >
                             <i className="fab text-light fa-linkedin"></i>
                             &nbsp; Dipesh Patil
