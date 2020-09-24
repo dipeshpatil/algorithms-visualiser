b0899b01252ee8c666e93585a9daf998151eb6c2
dipeshpatil <thedipeshpatil@gmail.com>
2020-09-24 18:58:50 +0530

    added thumbnails on homepage

--- src/utils/indexTable.jsx
+++ src/utils/indexTable.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import LinearThumb from "./thumbs/linear.jpg";
+import BinaryThumb from "./thumbs/binary.jpg";
+import NQueensThumb from "./thumbs/nqueens.jpg";
+import PathThumb from "./thumbs/path.jpg";
+import RatThumb from "./thumbs/rat.jpg";
+import SortingThumb from "./thumbs/sorting.jpg";
+
 import "./bootstrap.min.css";
 
 export default class IndexTable extends React.Component {
@@ -12,75 +19,73 @@ export default class IndexTable extends React.Component {
     render() {
         return (
             <div className="container mt-2">
-                <div className="text-center text-dark">So Far Implemented</div>
-                <table className="table table-striped table-light mt-2">
-                    <thead>
-                        <tr>
-                            <th scope="col">Latest Visualiser</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">URL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Pathfinding Visualiser</td>
-                            <td>Pathfinding</td>
-                            <td>
-                                <NavLink to="/pathfinder">
-                                    <code>/pathfinder</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Rat In A Maze</td>
-                            <td>Backtracking</td>
-                            <td>
-                                <NavLink to="/rat-in-a-maze">
-                                    <code>/rat-in-a-maze</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Sorting Visualiser</td>
-                            <td>Sorting</td>
-                            <td>
-                                <NavLink to="/sorting">
-                                    <code>/sorting</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>N Queens Problem</td>
-                            <td>Backtracking</td>
-                            <td>
-                                <NavLink to="/n-queens-problem">
-                                    <code>/n-queens-problem</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Binary Search</td>
-                            <td>Searching</td>
-                            <td>
-                                <NavLink to="/binary-search">
-                                    <code>/binary-search</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Linear Search</td>
-                            <td>Searching</td>
-                            <td>
-                                <NavLink to="/linear-search">
-                                    <code>/linear-search</code>
-                                </NavLink>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
                 <div className="text-center text-muted">
                     Happy Visualisation Day!
                 </div>
+                <center>
+                    <div className="row mt-2">
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/linear-search">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={LinearThumb}
+                                    alt="Linear Search"
+                                />
+                                <p className="text-dark thumb-title">Linear Search</p>
+                            </NavLink>
+                        </div>
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/binary-search">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={BinaryThumb}
+                                    alt="Binary Search"
+                                />
+                                <p className="text-dark thumb-title">Binary Search</p>
+                            </NavLink>
+                        </div>
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/n-queens-problem">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={NQueensThumb}
+                                    alt="NQueens"
+                                />
+                                <p className="text-dark thumb-title">NQueens Problem</p>
+                            </NavLink>
+                        </div>
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/pathfinder">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={PathThumb}
+                                    alt="Path Finding"
+                                />
+                                <p className="text-dark thumb-title">Path Finding</p>
+                            </NavLink>
+                        </div>
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/rat-in-a-maze">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={RatThumb}
+                                    alt="Rat In A Maze"
+                                />
+                                <p className="text-dark thumb-title">Rat In A Maze</p>
+                            </NavLink>
+                        </div>
+                        <div className="col-sm-4 col-6">
+                            <NavLink to="/sorting">
+                                <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={SortingThumb}
+                                    alt="Sorting"
+                                />
+                                <p className="text-dark thumb-title">Sorting Visualiser</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                </center>
             </div>
         );
     }
